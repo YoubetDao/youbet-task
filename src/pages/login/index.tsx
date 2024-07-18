403e0696290815f6fbbecc4afc21fd7ecc2e4ac2
Amateur <2863217986@qq.com>
2024-07-18 15:09:57 +0800

    feat: optimize login screen

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -16,48 +16,16 @@ export default function Login() {
         <div className="relative flex-col hidden h-full p-10 text-white bg-muted lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="w-6 h-6 mr-2"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Logo
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and helped me deliver stunning designs to my
-                clients faster than ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
+            <img src="/public/logo.png" alt="Icon" className="w-6 h-6 mr-2" />
+            You Bet
           </div>
         </div>
         <div className="flex items-center h-full p-4 lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">Create an account</h1>
-              <p className="text-sm text-muted-foreground">Enter your email below to create your account</p>
             </div>
             <UserAuthForm />
-            <p className="px-8 text-sm text-center text-muted-foreground">
-              By clicking continue, you agree to our{' '}
-              <Link to="/terms" className="underline underline-offset-4 hover:text-primary">
-                Terms of Service
-              </Link>{' '}
-              and{' '}
-              <Link to="/privacy" className="underline underline-offset-4 hover:text-primary">
-                Privacy Policy
-              </Link>
-              .
-            </p>
           </div>
         </div>
       </div>
