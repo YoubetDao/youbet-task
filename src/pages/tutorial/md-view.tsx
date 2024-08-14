978fb8d38b9b22ccaea23af24022f559f3867fed
Amateur0x1 <amateur11m11d@gmail.com>
2024-08-14 18:04:09 +0800

    feat: improve markdown renderer

--- src/pages/tutorial/md-view.tsx
+++ src/pages/tutorial/md-view.tsx
@@ -1,43 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react'
-import debounce from 'lodash/debounce'
+import React from 'react'
 import { useMd } from '@/components/md-renderer'
 interface IMarkdownRendererProps {
   content: string
 }
 
 const MdView = ({ content }: IMarkdownRendererProps) => {
-  const contentRef = useRef<HTMLElement>(null)
-  const [activeId, setActiveId] = useState('')
-  const { MdRenderer, TocSidebar } = useMd(content, { activeId: activeId, contentRef: contentRef })
-
-  const debouncedSetActiveId = debounce((id: string) => {
-    setActiveId(id)
-  }, 100) // 100ms 去抖动时间
-
-  useEffect(() => {
-    if (!contentRef.current) return
-
-    const observerOptions = {
-      root: null,
-      rootMargin: '0px 0px -70% 0px',
-      threshold: 1,
-    }
-
-    const observerCallback: IntersectionObserverCallback = (entries) => {
-      for (const entry of entries) {
-        if (entry.isIntersecting) {
-          debouncedSetActiveId(entry.target.id)
-          break
-        }
-      }
-    }
-
-    const observer = new IntersectionObserver(observerCallback, observerOptions)
-    const elements = contentRef.current.querySelectorAll('h1, h2, h3') as NodeListOf<HTMLElement>
-    elements.forEach((element) => observer.observe(element))
-
-    return () => observer.disconnect()
-  }, [debouncedSetActiveId])
+  const { MdRenderer, TocSidebar } = useMd(content, true)
 
   return (
     <div className="flex w-full justify-center">
