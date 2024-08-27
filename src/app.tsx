b9583b209fa407ea66755188f71b73ec0166fac4
wfnuser <wfnuser@hotmail.com>
2024-08-27 14:03:14 +0800

    feat: support edu chain

--- src/app.tsx
+++ src/app.tsx
@@ -6,14 +6,30 @@ import { createRouter } from './router'
 import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { WagmiProvider } from 'wagmi'
 import '@rainbow-me/rainbowkit/styles.css'
-import { scroll } from 'wagmi/chains'
 import { Provider } from 'jotai'
 import { store } from './store'
+
+// TODO: should be configured by the user
+const eduChain = {
+  id: 656476,
+  name: 'EduChain',
+  nativeCurrency: {
+    name: 'EDU',
+    symbol: 'EDU',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://open-campus-codex-sepolia.drpc.org'],
+      webSocket: ['wss://open-campus-codex-sepolia.drpc.org'],
+    },
+  },
+}
 
 const config = getDefaultConfig({
   appName: 'Kuibu',
   projectId: '05c3ea68819376e65dc4a8802f90f41b',
-  chains: [scroll],
+  chains: [eduChain],
   ssr: true, // If your dApp uses server side rendering (SSR)
 })
 
