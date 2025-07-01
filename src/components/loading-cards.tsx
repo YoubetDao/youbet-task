f330b7578e8e7db75ec50e5b30b5fe83d0ab4276
aw <757352776@qq.com>
2025-07-01 12:17:06 +0800

    feat: vertical skeleton (#398)

--- src/components/loading-cards.tsx
+++ src/components/loading-cards.tsx
@@ -2,24 +2,24 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 function SkeletonCard() {
   return (
-    <div className="flex flex-col space-y-3">
+    <>
       <Skeleton className="h-[125px] rounded-xl" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[500px]" />
+        <Skeleton className="h-4 w-[400px]" />
       </div>
-    </div>
+    </>
   )
 }
 
 export function LoadingCards({ count = 6 }: { count?: number }) {
   return (
-    <>
+    <div className="flex flex-col space-y-3 pt-4 lg:pl-4">
       {Array(count)
         .fill(0)
         .map((_, i) => (
           <SkeletonCard key={i} />
         ))}
-    </>
+    </div>
   )
 }
