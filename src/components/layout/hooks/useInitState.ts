1834c048baa49107dd57a755c73f82a3e18c1c11
Ming <757352776@qq.com>
2025-07-09 14:02:24 +0800

    fix: jotai useSetAtom

--- src/components/layout/hooks/useInitState.ts
+++ src/components/layout/hooks/useInitState.ts
@@ -1,16 +1,19 @@
 import { youbetApi } from '@/service'
 import { useUsername, walletAction, walletAtom } from '@/store'
 import { useAsyncEffect } from 'ahooks'
-import { useAtom } from 'jotai'
+import { useAtom, useSetAtom } from 'jotai'
 
 export const useInitState = () => {
   const [walletState] = useAtom(walletAtom)
-  const [, setWalletAddress] = useAtom(walletAction.changeLinkedAddress)
+  const setWalletAddress = useSetAtom(walletAction.changeLinkedAddress)
   const [github] = useUsername()
 
   useAsyncEffect(async () => {
-    if (walletState.linkedAddress === '' && github) {
-      const linkedAddress = await youbetApi.youbetControllerGetLinkedWallet(github).then((res) => res.data)
+    if (!github || walletState.linkedAddress) return
+
+    const linkedAddress = await youbetApi.youbetControllerGetLinkedWallet(github).then((res) => res.data)
+
+    if (linkedAddress) {
       setWalletAddress(linkedAddress)
     }
   }, [walletState.linkedAddress, github])
