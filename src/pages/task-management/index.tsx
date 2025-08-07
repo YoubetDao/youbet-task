3a4c0998db446f826c0af1d4891105eb236f0dda
aw <19972938+Bystander2@users.noreply.github.com>
2025-08-07 21:16:37 +0800

    feat: sort due, priority and sp

--- src/pages/task-management/index.tsx
+++ src/pages/task-management/index.tsx
@@ -8,11 +8,15 @@ import { useState } from 'react'
 import { PAGESIZE, STALETIME } from '@/constants/contracts/request'
 import { useQuery } from '@tanstack/react-query'
 import TaskMgtTable from './_components/TaskMgtTable'
+import { ISort } from './_components/TableSortHeader'
 
 export default function TaskManagement() {
   const [page, setPage] = useState(1)
+  const [sort, setSort] = useState<ISort[]>([])
 
-  const queryKey = ['tasks', '', page]
+  const sortParams = sort.map((item) => `${item.field}:${item.value}`).join(',')
+
+  const queryKey = ['tasks', '', page, sortParams]
   const queryFn = () =>
     taskApi
       .taskControllerGetManagedTasks(
@@ -26,6 +30,7 @@ export default function TaskManagement() {
         TaskControllerGetTasksNoGrantNeededEnum.All,
         (page - 1) * PAGESIZE,
         PAGESIZE,
+        sortParams,
       )
       .then((res) => res.data)
 
@@ -41,7 +46,7 @@ export default function TaskManagement() {
 
   return (
     <div className="space-y-4">
-      <TaskMgtTable tasks={tasks} page={page} totalPages={totalPages} setPage={setPage} />
+      <TaskMgtTable tasks={tasks} page={page} totalPages={totalPages} setPage={setPage} sort={sort} setSort={setSort} />
     </div>
   )
 }
